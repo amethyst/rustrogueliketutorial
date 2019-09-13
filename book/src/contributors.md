6745c17e095e6ec71676ac948d22a150210bd94b
Herbert Wolverson <herberticus@gmail.com>
2019-09-13 08:35:14 -0500

    Apply visibility fix, check in multiple chapters. Add contributors page. Fix a busted reference to distance2d from a previous RLTK version.

---
+++ book/src/contributors.md
@@ -0,0 +1,22 @@
+# Contributors
+
+---
+
+***About this tutorial***
+
+*This tutorial is free and open source, and all code uses the MIT license - so you are free to do with it as you like. My hope is that you will enjoy the tutorial, and make great games!*
+
+*If you enjoy this and would like me to keep writing, please consider supporting [my Patreon](https://www.patreon.com/blackfuture).*
+
+---
+
+The following people have contributed to this project:
+
+* Herbert Wolverson, the primary author.
+* [Marius Gedminas](https://github.com/mgedmin) provided some fixes to the visibility system, and chapter 5.
+
+---
+
+Copyright (C) 2019, Herbert Wolverson.
+
+---
--- chapter-05-fov/src/visibility_system.rs
+++ chapter-05-fov/src/visibility_system.rs
@@ -17,8 +17,7 @@ impl<'a> System<'a> for VisibilitySystem {
         let (mut map, entities, mut viewshed, pos, player) = data;
 
         for (ent,viewshed,pos) in (&entities, &mut viewshed, &pos).join() {
-            if viewshed.dirty {
-                for t in map.visible_tiles.iter_mut() { *t = false };
+            if viewshed.dirty {                
                 viewshed.dirty = false;
                 viewshed.visible_tiles.clear();
                 viewshed.visible_tiles = field_of_view(Point::new(pos.x, pos.y), viewshed.range, &*map);
@@ -26,6 +25,7 @@ impl<'a> System<'a> for VisibilitySystem {
                 // If this is the player, reveal what they can see
                 let _p : Option<&Player> = player.get(ent);
                 if let Some(_p) = _p {
+                    for t in map.visible_tiles.iter_mut() { *t = false };
                     for vis in viewshed.visible_tiles.iter() {
                         let idx = map.xy_idx(vis.x, vis.y);
                         map.revealed_tiles[idx] = true;
