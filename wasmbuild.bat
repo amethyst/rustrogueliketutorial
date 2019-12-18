3cce1f00d176aee9f0f568be167e520402c6f194
Herbert Wolverson <herberticus@gmail.com>
2019-12-18 11:51:04 -0600

    #50 - Clean up some errors from my last commit.

--- wasmbuild.bat
+++ wasmbuild.bat
@@ -76,6 +76,7 @@ REM Usage: Stage Chapter
 :Stage
 cd %~1
 cargo build --release --target wasm32-unknown-unknown
+if %errorlevel% neq 0 exit /b %errorlevel%
 wasm-bindgen ..\target\wasm32-unknown-unknown\release\%~1.wasm --out-dir ../book/book/wasm/%~1 --no-modules --no-typescript
 cd ..
 move .\book\book\wasm\%~1\%~1.js .\book\book\wasm\%~1\myblob.js
