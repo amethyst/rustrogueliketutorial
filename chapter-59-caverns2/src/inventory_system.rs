9b68100f06b231bca775cf06439352a05d2b81ee
Niels Renard <code.i.pushed@gmail.com>
2020-01-12 00:25:43 +0900

    Fix capitalization

--- chapter-59-caverns2/src/inventory_system.rs
+++ chapter-59-caverns2/src/inventory_system.rs
@@ -291,7 +291,7 @@ impl<'a> System<'a> for ItemDropSystem {
             dirty.insert(entity, EquipmentChanged{}).expect("Unable to insert");
 
             if entity == *player_entity {
-                gamelog.entries.insert(0, format!("you drop the {}.", names.get(to_drop.item).unwrap().name));
+                gamelog.entries.insert(0, format!("You drop the.", names.get(to_drop.item).unwrap().name));
             }
         }
 
