533f6ab970f8155e5f0bd53b1ee231139a822e8c
Herbert Wolverson <herberticus@gmail.com>
2020-04-11 09:51:28 -0500

    #87 - Use VISIBLE not revealed tiles for tooltip revelation.

--- chapter-11-loadsave/src/gui.rs
+++ chapter-11-loadsave/src/gui.rs
@@ -38,7 +38,7 @@ fn draw_tooltips(ecs: &World, ctx : &mut Rltk) {
     let mut tooltip : Vec<String> = Vec::new();
     for (name, position) in (&names, &positions).join() {
         let idx = map.xy_idx(position.x, position.y);
-        if position.x == mouse_pos.0 && position.y == mouse_pos.1 && map.revealed_tiles[idx] {
+        if position.x == mouse_pos.0 && position.y == mouse_pos.1 && map.visible_tiles[idx] {
             tooltip.push(name.name.to_string());
         }
     }
