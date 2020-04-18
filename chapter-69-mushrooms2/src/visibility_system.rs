86d38d3d0cc72acca23c8cc7d36e64b49e6be28f
Herbert Wolverson <herberticus@gmail.com>
2020-04-18 10:52:22 -0500

    #59 #115 #57 - Chapter 69 spatial fixes.

--- chapter-69-mushrooms2/src/visibility_system.rs
+++ chapter-69-mushrooms2/src/visibility_system.rs
@@ -44,18 +44,18 @@ impl<'a> System<'a> for VisibilitySystem {
                             map.visible_tiles[idx] = true;
 
                             // Chance to reveal hidden things
-                            for e in map.tile_content[idx].iter() {
-                                let maybe_hidden = hidden.get(*e);
+                            crate::spatial::for_each_tile_content(idx, |e| {
+                                let maybe_hidden = hidden.get(e);
                                 if let Some(_maybe_hidden) = maybe_hidden {
                                     if rng.roll_dice(1,24)==1 {
-                                        let name = names.get(*e);
+                                        let name = names.get(e);
                                         if let Some(name) = name {
                                             log.entries.push(format!("You spotted a {}.", &name.name));
                                         }
-                                        hidden.remove(*e);
+                                        hidden.remove(e);
                                     }
                                 }
-                            }
+                            });
                         }
                     }
                 }
