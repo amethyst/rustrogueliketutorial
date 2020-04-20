3fdddca55ccf101c670ae2c590274db5c32c3b33
Herbert Wolverson <herberticus@gmail.com>
2020-04-20 12:08:05 -0500

    Apply multi-tile fix to subsequent chapters.

--- chapter-69-mushrooms2/src/map_indexing_system.rs
+++ chapter-69-mushrooms2/src/map_indexing_system.rs
@@ -1,17 +1,19 @@
 use specs::prelude::*;
-use super::{Map, Position, BlocksTile, Pools, spatial};
+use super::{Map, Position, BlocksTile, Pools, spatial, TileSize};
 
 pub struct MapIndexingSystem {}
 
 impl<'a> System<'a> for MapIndexingSystem {
+    #[allow(clippy::type_complexity)]
     type SystemData = ( ReadExpect<'a, Map>,
                         ReadStorage<'a, Position>,
                         ReadStorage<'a, BlocksTile>,
                         ReadStorage<'a, Pools>,
+                        ReadStorage<'a, TileSize>,
                         Entities<'a>,);
 
     fn run(&mut self, data : Self::SystemData) {
-        let (map, position, blockers, pools, entities) = data;
+        let (map, position, blockers, pools, sizes, entities) = data;
 
         spatial::clear();
         spatial::populate_blocked_from_map(&*map);
@@ -23,8 +25,21 @@ impl<'a> System<'a> for MapIndexingSystem {
                 }
             }
             if alive {
-                let idx = map.xy_idx(position.x, position.y);
-                spatial::index_entity(entity, idx, blockers.get(entity).is_some());
+                if let Some(size) = sizes.get(entity) {
+                    // Multi-tile
+                    for y in position.y .. position.y + size.y {
+                        for x in position.x .. position.x + size.x {
+                            if x > 0 && x < map.width-1 && y > 0 && y < map.height-1 {
+                                let idx = map.xy_idx(x, y);
+                                spatial::index_entity(entity, idx, blockers.get(entity).is_some());
+                            }
+                        }
+                    }
+                } else {
+                    // Single tile
+                    let idx = map.xy_idx(position.x, position.y);
+                    spatial::index_entity(entity, idx, blockers.get(entity).is_some());
+                }
             }
         }
     }
