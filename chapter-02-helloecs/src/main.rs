3496aad3889b276faea2a46d9b6412b6fe2be738
Xiayun Sun <xiayunsun@gmail.com>
2022-07-31 23:46:42 +0100

    remove unnecessary mut borrow and write access

--- chapter-02-helloecs/src/main.rs
+++ chapter-02-helloecs/src/main.rs
@@ -28,15 +28,15 @@ struct State {
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
+    let players = ecs.read_storage::<Player>();
 
-    for (_player, pos) in (&mut players, &mut positions).join() {
+    for (_player, pos) in (&players, &mut positions).join() {
         pos.x = min(79 , max(0, pos.x + delta_x));
         pos.y = min(49, max(0, pos.y + delta_y));
     }
 }
 
-fn player_input(gs: &mut State, ctx: &mut Rltk) {
+fn player_input(gs: &mut State, ctx: &Rltk) {
     // Player movement
     match ctx.key {
         None => {} // Nothing happened
